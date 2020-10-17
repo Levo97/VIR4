7ff06acb62a87c32d112e00519e9cadcd1fc4b28
Levo97 <kocsorlevente@outlook.com>
2020-10-17 17:45:42 +0200

    Java 8 version

---
+++ src/main/java/FM9IM6/VIR4/Vir4Application.java
@@ -0,0 +1,13 @@
+package FM9IM6.VIR4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vir4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vir4Application.class, args);
+	}
+
+}
